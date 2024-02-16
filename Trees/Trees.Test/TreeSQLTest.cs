370829e96536a199b70708ccefdfefe256d3779a
davidkhachatryan123 <davidkhachatryan359@gmail.com>
2024-02-16 20:22:12 +0400

    feat: Add StopWatch in tests

--- Trees/Trees.Test/TreeSQLTest.cs
+++ Trees/Trees.Test/TreeSQLTest.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Trees.Test.Data;
 
@@ -13,17 +14,24 @@ public class TreeSQLTest(WebApplicationFactory<TreeSQL.Program> factory)
     public async void IsChildOf(int parent_ID, int[] children_IDs)
     {
         // Arrange
+        Stopwatch stopwatch = new();
         var client = _factory.CreateClient();
 
         foreach (int childID in children_IDs)
         {
             // Act
+            stopwatch.Start();
             var response = await client.GetAsync($"/api/Tree/{childID}/IsChildOf?parnetId={parent_ID}");
+            stopwatch.Stop();
 
             // Assert
             response.EnsureSuccessStatusCode();
             string result = await response.Content.ReadAsStringAsync();
             Assert.Equal("true", result);
         }
+
+        var time = (double)stopwatch.ElapsedMilliseconds;
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.WriteLine($"| SQL | {time} ms |");
     }
 }
