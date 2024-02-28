d6b5267f13f30f45831f7e02ee10d4683c2e4be6
davidkhachatryan123 <davidkhachatryan359@gmail.com>
2024-02-28 19:00:24 +0400

    chore: Remove StopWatch from tests

--- Trees/Trees.Test/TreeElasticStackTest.cs
+++ Trees/Trees.Test/TreeElasticStackTest.cs
@@ -14,24 +14,17 @@ public class TreeElasticStackTest(WebApplicationFactory<TreeElasticStack.Program
   public async void IsChildOf(string parent_ID, string[] children_IDs)
   {
     // Arrange
-    Stopwatch stopwatch = new();
     var client = _factory.CreateClient();
 
     foreach (string childID in children_IDs)
     {
       // Act
-      stopwatch.Start();
       var response = await client.GetAsync($"/api/Tree/{childID}/IsChildOf?parnetId={parent_ID}");
-      stopwatch.Stop();
 
       // Assert
       response.EnsureSuccessStatusCode();
       string result = await response.Content.ReadAsStringAsync();
       Assert.Equal("true", result);
     }
-
-    var time = (double)stopwatch.ElapsedMilliseconds;
-    Console.ForegroundColor = ConsoleColor.Magenta;
-    Console.WriteLine($"| ElasticStack | {time} ms |");
   }
 }
