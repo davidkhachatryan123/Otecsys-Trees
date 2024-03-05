66eccdc2daae6145e89761da28bc7edc55509c43
davidkhachatryan123 <davidkhachatryan359@gmail.com>
2024-03-05 08:51:19 +0400

    refactor: Make CheckAccess method asynchronous in mongo based sln

--- src/MongoDB/Services/OrganizationHelperService.cs
+++ src/MongoDB/Services/OrganizationHelperService.cs
@@ -1,22 +1,24 @@
-﻿using MongoDB.Bson;
+﻿using Common.Interfaces;
+using MongoDB.Bson;
 using MongoDB.Driver;
 using MongoDB.Models;
 
 namespace MongoDB.Services;
 
 public class OrganizationHelperService(IMongoClient mongoClient)
+  : IOrganizationOperations<string>
 {
   private readonly IMongoCollection<Organization> _organizations = mongoClient
     .GetDatabase("organizationdb")
     .GetCollection<Organization>("organizations");
 
-  public bool CheckAccess(string nodeId, string parentId)
+  public async Task<bool> CheckAccess(string nodeId, string parentId)
   {
     try
     {
-      return _organizations.Find(org => org.Id == ObjectId.Parse(nodeId))
-                         .FirstOrDefault()
-                         .Ancestors.Any(a => a == ObjectId.Parse(parentId));
+      return (await _organizations.Find(org => org.Id == ObjectId.Parse(nodeId))
+                                  .FirstOrDefaultAsync())
+                                  .Ancestors.Any(a => a == ObjectId.Parse(parentId));
     }
     catch { return false; }
   }
